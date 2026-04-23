c09cc86bcaa0c25515256694d8eefa4aa677988b
Will Giles <240115551@aston.ac.uk>
2026-04-23 14:32:27 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/CoffeeBarCo/">
+      <BrowserRouter>
         <ScrollToTop />
         <AnimatedRoutes />
       </BrowserRouter>
